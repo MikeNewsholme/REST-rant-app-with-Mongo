314ab088d17455c8d47439f374cabf47dba4f72f
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-06-29 10:44:51 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
+
+app.use('/places', require('./controllers/places'))
+
 app.get("/", function (res, req) {
   res.setEncoding("Hello World!");
 });
+
 app.get('*', (req,res) =>{
     res.status(404).send('<h1> 404 Page</h1>')
 })
+
 app.listen(process.env.PORT);
