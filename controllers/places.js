1ca353f03286fdc3b6be8a2b3bdd84f829b96ca8
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-07-01 10:48:45 -0400

    added places view with kittens because NCSU loves kittens

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("GET /places");
+  let places = [{
+    name: 'H-Thai-ML',
+    city: "Seattle",
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'Http://placekitten.com/250/250'
+
+  }, {
+    name:'Coding Cat Cafe',
+    city: "Phoenix",
+    state:'AZ',
+    cuisines: "coffee, Bakery",
+    pic:'http://placekitten.com/250/250'
+  }]
+  res.render('places/index', {places})
+  //res.send("GET /places");
 });
+// GET /places
+
+
 
 module.exports = router;
---
+++ views/places/index.jsx
@@ -0,0 +1,28 @@
+const React = require('react')
+const Def = require('../default')
+
+function index(data){
+    let placesFormatted = data.places.map((place)=>{
+    return (
+        <div>
+            <h2>{place.name}</h2>
+            <img src={place.pic} alt={place.name}></img>
+        </div>
+
+    )
+    
+    
+    })
+        return (
+        
+        <Def>
+            <main>
+                <h1>This is the places index view</h1>
+                {placesFormatted}
+            </main>
+        </Def>
+    )
+ 
+}
+
+module.exports = index
