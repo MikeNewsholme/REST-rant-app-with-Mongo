41a2b653c0cadb1182dc22f489f8ced33ad3dc05
mike.newsholme@gmail.com <mike.newsholme@gmail.com>
2022-07-15 09:49:24 -0400

    comments are returning undefined, need to get help

--- controllers/places.js
+++ controllers/places.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
 const express = require("express");
-const db = require('../models')
+const { trusted } = require("mongoose");
+const db = require("../models");
 
 //INDEX ROUTE
 router.get("/", (req, res) => {
   db.Place.find()
-  .then((places)=>{
-    res.render('places/index', { places })
-  })
-  .catch(err =>{
-    console.log(err)
-    res.render('error404')
-  })
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("error404");
+    });
 });
 //POST ROUTE
 router.post("/", (req, res) => {
@@ -30,21 +31,23 @@ router.post("/", (req, res) => {
     });
 });
 
-
 //NEW
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
+//show
 router.get("/:id", (req, res) => {
   db.Place.findById(req.params.id)
-  .then(place =>{
-    res.render('places/show', { place })
-  })
-  .catch(err => {
-    console.log('err', err)
-    res.render('error404')
-  })
+    .populate("comments")
+    .then((place) => {
+      console.log(place.comments);
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
 
 router.put("/:id", (req, res) => {
@@ -61,12 +64,38 @@ router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
 
-router.post("/:id/rant", (req, res) => {
-  res.send("GET /places/:id/rant stub");
-});
-
 router.delete("/:id/rant/:rantId", (req, res) => {
   res.send("GET /places/:id/rant/:rantId stub");
+});
+
+//Rants
+router.post("/:id/comment", (req, res) => {
+  console.log(req.params.id);
+  req.body.rant = req.body.rant ? true : false;
+  if (req.body.author === "") {
+    req.body.author = undefined;
+  }
+  if (req.body.content === "") {
+    req.body.content = undefined;
+  }
+  console.log(req.body);
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      db.Comment.create(req.body)
+        .then((comment) => {
+          place.comments.push(comment.id);
+          place.save().then(() => {
+            res.redirect(`/places/${req.params.id}`);
+          });
+        })
+        .catch((err) => {
+          res.render("error404");
+        });
+    })
+    .catch((err) => {
+      res.render("error404");
+    });
+  //res.send('POST /places/:id/comment stub')
 });
 
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,52 +2,132 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+  let comments = (
+    <h3 className='inactive'>
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length){
+    comments = data.place.comments.map(c => {
+      return (
+        <div className='border'>
+          <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave'}
+          </h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong>- {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
       <Def>
         <main>
           <div className="row">
             <div className="col-sm-6">
-              <img src={data.place.name} alt={data.place.name} />
+              <img className="img-fluid" src={data.place.pic} alt={data.place.name} />
               <h3>
                 Located in {data.place.city}, {data.place.state}
               </h3>
-              <h2>
-                <img
-                  className="showImage"
-                  src={data.place.pic}
-                  alt="Show Food"
-                />
-              </h2>
             </div>
-          </div>
-          <h2>Rating</h2>
-          <div>
-            <p>Not Rated</p>
-          </div>
-          <h2>Description</h2>
-          <div>
-           <h3> 
-            {data.place.showEstablished()}
-           </h3>
-           <h4>
-            Serving {data.place.cuisines}
-           </h4>
-          </div>
-          <h2>Comments</h2>
-          <div>
-            <p>No Comments Yet</p>
-          </div>
-          <div>
-            <h3>
+            <div className="col-sm-6 show-box">
+              <h1>{data.place.name}</h1>
+              <div>
+                <h2>Rating</h2>
+                
+              </div>
+              <div>
+                <h2>Desciption</h2>
+                <h3>{data.place.showEstablished()}</h3>
+                <h4>Serving {data.place.cuisines}</h4>
+              </div>
               <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                 Edit
               </a>
-              <form method="POST" action={`places/${data.id}?_method=DELETE`}>
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
                 <button type="submit" className="btn btn-danger">
                   Delete
                 </button>
               </form>
-            </h3>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="row comment-box">
+                <h2>
+                  <br />
+                  Comments
+                </h2>
+                {comments}
+              </div>
+              <hr />
+              <div className="rant-post-box">
+                <h1>Got Your Own Rant or Rave?</h1>
+                <form
+                  className="row g-3"
+                  method="POST"
+                  action={`/places/${data.id}/comment`}
+                >
+                  <div className="col-12">
+                    <label htmlFor="content" className="form-label">
+                      Your Comment
+                    </label>
+                    <textarea
+                      className="form-control"
+                       type="text"
+                      id="content"
+                      name="content"
+                    ></textarea>
+                  </div>
+                  <div className="col-md-4">
+                    <label htmlFor="author" className="form-label">
+                      Your Name
+                    </label>
+                    <input className="form-control" id="author" name="author" />
+                  </div>
+                  <div className="col-md-4">
+                    <label htmlFor="stars" className="form-label">
+                      Stars
+                    </label>
+                    <input
+                      className="form-range"
+                      type="range"
+                      id="stars"
+                      name="stars"
+                      min="1"
+                      max="5"
+                      step="0.5"
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    <div className="form-check">
+                      <label
+                        className="form-check-label"
+                        htmlFor="rant"
+                        id="rant-checkbox"
+                      >
+                        Rant?
+                      </label>
+                      <br />
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        id="rant"
+                        name="rant"
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      className="btn btn-primary"
+                      type="submit"
+                      value="Add Comment"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </main>
       </Def>
